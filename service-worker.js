7e5d1977fd8946b38e06173477ddf61f1c890d78
Taras Konovalov <taras.konovalov2014@gmail.com>
2021-05-25 15:09:10 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.5040cb1dade87a789d67a2e3af38ecb4.js"
 );
 
 self.addEventListener('message', (event) => {
